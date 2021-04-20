8c2b2aab1720c4a2925c20d6b2c64c5586f39efb
unknown <ibmjavanh09@iiht.tech>
2021-04-20 11:50:14 +0530

    Added Create Search navigtion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BugFormComponent } from './bugform/bugform.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CreateBugComponent } from './create-bug/create-bug.component';
+import { SearchBugComponent } from './search-bug/search-bug.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BugFormComponent,
+    CreateBugComponent,
+    SearchBugComponent,
   ],
   imports: [
     BrowserModule,FormsModule,HttpClientModule,
---
+++ src/app/search-bug/search-bug.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bug',
+  templateUrl: './search-bug.component.html',
+  styleUrls: ['./search-bug.component.css']
+})
+export class SearchBugComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
